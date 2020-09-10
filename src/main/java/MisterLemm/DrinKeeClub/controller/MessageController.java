b16be9855ab9bb93d97f8e2a55d80ea22d4b3c3b
MisterLemm <Keonied@gmail.com>
2020-09-10 11:57:07 +0400

    Db connection, MessageRepository, fixed a bug with existed id upon updating message

---
+++ src/main/java/MisterLemm/DrinKeeClub/controller/MessageController.java
@@ -0,0 +1,59 @@
+package MisterLemm.DrinKeeClub.controller;
+
+import MisterLemm.DrinKeeClub.domain.Message;
+import MisterLemm.DrinKeeClub.domain.Views;
+import MisterLemm.DrinKeeClub.repository.MessageRepository;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+
+    private final MessageRepository messageRepository;
+
+    @Autowired
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @GetMapping
+    @JsonView(Views.IdName.class)
+    public List<Message> list(){
+        return messageRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Message getOne(
+            @PathVariable("id") Message message
+    ){
+        return message;
+    }
+
+    @PostMapping
+    public Message create(
+            @RequestBody Message message
+    ){
+        message.setCreationDate(LocalDateTime.now());
+        return messageRepository.save(message);
+    }
+
+    @PutMapping("{id}")
+    public Message update(
+            @PathVariable("id") Message messageFromDb,
+            @RequestBody Message message
+    ){
+        BeanUtils.copyProperties(message, messageFromDb, "id");
+            return messageRepository.save(messageFromDb);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Message message){
+        messageRepository.delete(message);
+    }
+}
---
+++ src/main/java/MisterLemm/DrinKeeClub/domain/Views.java
@@ -0,0 +1,7 @@
+package MisterLemm.DrinKeeClub.domain;
+
+public final class Views {
+    public interface IdName {}
+
+    public interface FullMessage extends IdName {}
+}
