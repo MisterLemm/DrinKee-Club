eff1135541b7c5fbc27b265666d51520bf5d93f4
MisterLemm <Keonied@gmail.com>
2020-09-10 11:55:08 +0400

    Db connection, MessageRepository, fixed a bug with existed id upon updating message

--- src/main/java/MisterLemm/DrinKeeClub/domain/Views.java
+++
@@ -1,7 +0,0 @@
-package MisterLemm.DrinKeeClub.domain;
-
-public final class Views {
-    public interface IdName {}
-
-    public interface FullMessage extends IdName {}
-}
--- src/main/java/MisterLemm/DrinKeeClub/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package MisterLemm.DrinKeeClub.repository;
-
-import MisterLemm.DrinKeeClub.domain.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
